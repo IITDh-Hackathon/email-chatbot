eb90a49003937c03158143d3f684f423b94ca861
karthikmurakonda <karthik.murakonda14@gmail.com>
2024-02-03 17:46:38 +0530

    added chromadb

--- services/sync_service.py
+++ services/sync_service.py
@@ -1,4 +1,5 @@
 import os
+import uuid
 
 from simplegmail import Gmail
 import sqlalchemy
@@ -9,6 +10,10 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+from langchain_openai import OpenAIEmbeddings
+import chromadb
+from ..src.constants import *
+
 # from langchain.embeddings import Embeddings
 
 from dotenv import load_dotenv
@@ -17,6 +22,8 @@ Base = sqlalchemy.orm.declarative_base()
 
 # global variables
 session = None
+chromadb_instance = None
+embeddings = OpenAIEmbeddings()
 
 def load_env():
     load_dotenv("../.env")
@@ -28,6 +35,14 @@ def make_db_session():
         Base.metadata.create_all(engine)
         Session = sessionmaker(bind=engine)
         session = Session()
+    except Exception as e:
+        print(e)
+
+def make_chroma_session():
+    global chromadb_instance
+    try:
+        chromadb_instance = chromadb.PersistentClient(path=os.getenv("CHROMA_DB_PATH"))
+
     except Exception as e:
         print(e)
 
@@ -74,13 +89,31 @@ def sync_emails(gmail_client : Gmail):
     add_to_db(emails)
 
 
+def split_mail(mail):
+    pass
+
+def embed_mail(chunks):
+    return embeddings.embed_documents(chunks)
+
+
+def store_embeddings(chunks, embeddings, metadata):
+    collection = chromadb_instance.get_or_create_collection(COLLECTION_NAME,
+                                                        metadata={"hnsw:space": SIMILARITY_SEARCH_TYPE})
+    ids = [str(uuid.uuid1()) for _ in range(len(chunks))]
+    for i, chunk in enumerate(chunks):
+        collection.add(documents=chunk, embeddings=embeddings[i], metadata={"id": ids[i]})
+    return ids
+
+
 def get_db_emails():
     return session.query(Mail).all()
 
 if __name__ == "__main__":
     load_env()
     make_db_session()
+    make_chroma_session()
     gmail = Gmail()
-    sync_emails(gmail)
-    print(get_db_emails())
+    # sync_emails(gmail)
+    # print(get_db_emails())
+
 
