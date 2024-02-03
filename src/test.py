7f343f0755e6bbe1638ddaa94bd3e5082d3cceb9
karthik-k-18 <200010024@iitdh.ac.in>
2024-02-04 02:00:39 +0530

    changes gmail token path

--- src/test.py
+++ src/test.py
@@ -4,13 +4,18 @@ from textual.widgets import Static,Collapsible, Footer, Label, Markdown
 
 class HorizontalLayoutExample(App):
     CSS_PATH = "grid.tcss"
-
     def compose(self) -> ComposeResult:
         yield Static("One", classes="box")
         yield Static("Two", classes="box")
         yield Static("Three", classes="box")
+        
+class MainApp(App):
+    
+    def compose(self)->ComposeResult:
+        yield HorizontalLayoutExample()
+    
 
 
 if __name__ == "__main__":
-    app = HorizontalLayoutExample()
+    app = MainApp()
     app.run()
