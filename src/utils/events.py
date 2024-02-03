83d075b43c80b57efe05ac599c4b8e280229ba5b
mr-rajashekhar <tella.rajashekhar@hmail.com>
2024-02-04 04:29:12 +0530

    added get email

--- src/utils/events.py
+++ src/utils/events.py
@@ -47,3 +47,41 @@ def get_events():
 # Close the cursor and connection
 # print(json.dumps(events))
 
+def get_mails():
+    table_name = "mail"
+
+    # Connect to the PostgreSQL database
+    conn = psycopg2.connect(
+        dbname=dbname, user=user, password=password, host=host, port=port
+    )
+
+    # Create a cursor
+    cursor = conn.cursor()
+
+    # Construct and execute a SQL query to select data from the table
+    query = sql.SQL("SELECT * FROM {}").format(sql.Identifier(table_name))
+    cursor.execute(query)
+
+    # Fetch all rows
+    rows = cursor.fetchall()
+
+    # Print the fetched data
+    emails = []
+    count = 0
+    for my_tuple in rows:
+        if count == 15:
+            break
+        result_dict = {
+        "from": my_tuple[2],
+        "time": my_tuple[3] +"-"+ my_tuple[2],
+        "subject": my_tuple[4],
+        "message": my_tuple[6]
+        }
+        emails.append(result_dict)
+        count += 1
+    cursor.close()
+    conn.close()
+    print(emails)
+    return emails
+
+get_mails()
