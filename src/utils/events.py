2372feeb82e7d12e9e427bd90e3571bffcb2e3ed
mr-rajashekhar <tella.rajashekhar@hmail.com>
2024-02-04 03:11:45 +0530

    added get events

--- src/utils/events.py
+++ src/utils/events.py
@@ -1,3 +1,49 @@
+import psycopg2
+from psycopg2 import sql
+import json
 
-def get_events():
-    pass
+# Replace these values with your PostgreSQL connection details
+dbname = "mail"
+user = "postgres"
+password = "karthik"
+host = "localhost"
+port = "5432"
+
+# Specify the table name you want to read from
+def dispaly_events():
+    table_name = "event"
+
+    # Connect to the PostgreSQL database
+    conn = psycopg2.connect(
+        dbname=dbname, user=user, password=password, host=host, port=port
+    )
+
+    # Create a cursor
+    cursor = conn.cursor()
+
+    # Construct and execute a SQL query to select data from the table
+    query = sql.SQL("SELECT * FROM {}").format(sql.Identifier(table_name))
+    cursor.execute(query)
+
+    # Fetch all rows
+    rows = cursor.fetchall()
+
+    # Print the fetched data
+    events = []
+    count = 0
+    for my_tuple in rows:
+        if count == 10:
+            break
+        result_dict = {
+        "Event": my_tuple[1],
+        "Time": my_tuple[3] +"-"+ my_tuple[2]
+        }
+        events.append(result_dict)
+        count += 1
+    cursor.close()
+    conn.close()
+    return events
+
+# Close the cursor and connection
+# print(json.dumps(events))
+
