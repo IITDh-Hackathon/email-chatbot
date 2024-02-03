81739e64183483360ac768de607784314eae1cd7
karthikmurakonda <karthik.murakonda14@gmail.com>
2024-02-03 16:26:30 +0530

    refractor service

--- services/sync_service.py
+++ services/sync_service.py
@@ -9,13 +9,31 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+# from langchain.embeddings import Embeddings
+
 from dotenv import load_dotenv
 
 Base = sqlalchemy.orm.declarative_base()
 
-load_dotenv("../.env")
+# global variables
+session = None
+
+def load_env():
+    load_dotenv("../.env")
+
+def make_db_session():
+    global session
+    try:
+        engine = create_engine(os.getenv("DB_URI"))
+        Base.metadata.create_all(engine)
+        Session = sessionmaker(bind=engine)
+        session = Session()
+    except Exception as e:
+        print(e)
 
 
+
+# table for mails in the database
 class Mail(Base):
     __tablename__ = 'mail'
 
@@ -26,59 +44,40 @@ class Mail(Base):
     subject = Column(String)
     date = Column(String)
     message = Column(String)
-    # labels = Column(String)
 
-
-# host=localhost port=5432 dbname=cdc user=postgres password=xxxxxxx sslmode=prefer connect_timeout=10
-# engine = create_engine('postgresql://postgres:karthik@localhost:5432/mail')
-# Session = sessionmaker(bind=engine)
-# session = Session()
-
-try:
-    engine = create_engine(os.getenv("DB_URI"))
-    Base.metadata.create_all(engine)
-    Session = sessionmaker(bind=engine)
-    session = Session()
-except Exception as e:
-    print(e)
+    def __repr__(self):
+        return f"<Mail(message_id={self.message_id}, sender={self.sender}, recipient={self.recipient}, subject={self.subject}, date={self.date}, message={self.message})>"
 
 
 gmail = Gmail()
 
+def sync_emails():
+    # messages = gmail.get_unread_messages(query=construct_query(query_params))
+    query_params = {
+        "newer_than": (2, "day"),
+    }
+    emails = gmail.get_messages(query=construct_query(query_params))
+
+    for message in emails:
+        mail = Mail(
+            message_id=str(message.id),
+            thread_id=str(message.thread_id),
+            sender=message.sender,
+            recipient=message.recipient,
+            subject=message.subject,
+            date=message.date,
+            message=message.plain,
+            # labels=str(message.labels)
+        )
+        session.add(mail)
+        session.commit()    
 
 
-# messages = gmail.get_unread_messages(query=construct_query(query_params))
+def get_emails():
+    return session.query(Mail).all()
 
-query_params = {
-    "newer_than": (2, "day"),
-}
-# emails = gmail.get_messages(query=construct_query(query_params))
+if __name__ == "__main__":
+    load_env()
+    make_db_session()
+    print(get_emails())
 
-# for message in emails:
-#     mail = Mail(
-#         message_id=str(message.id),
-#         thread_id=str(message.thread_id),
-#         sender=message.sender,
-#         recipient=message.recipient,
-#         subject=message.subject,
-#         date=message.date,
-#         message=message.plain,
-#         # labels=str(message.labels)
-#     )
-#     session.add(mail)
-#     session.commit()
-
-
-result = session.query(Mail).all()
-i = 0
-for row in result:
-    i+=1
-    print("To: " + row.recipient)
-    print("From: " + row.sender)
-    print("Subject: " + row.subject)
-    print("Date: " + row.date)
-    print("Thread ID: " + row.thread_id)
-    print("Message ID: " + row.message_id)
-    # print("Message: " + row.message)
-    # print("Labels: " + row.labels)
-print(i)
