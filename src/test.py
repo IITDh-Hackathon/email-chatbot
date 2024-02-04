e372161006e4ffa1b1626db99646b20244492bfb
karthikmurakonda <karthik.murakonda14@gmail.com>
2024-02-04 05:39:29 +0530

    basic layout for chat

--- src/test.py
+++ src/test.py
@@ -1,6 +1,6 @@
 from textual.app import App, ComposeResult
-from textual.widgets import Static,Collapsible, Footer, Label, Markdown, DataTable
-from textual.containers import ScrollableContainer
+from textual.widgets import Static,Collapsible, Footer, Label, Markdown, DataTable, TextArea
+from textual.containers import ScrollableContainer, Vertical
 
 LETO = """\
 # Duke Leto I Atreides
@@ -55,15 +55,47 @@ class CollapsibleApp(Static):
         for child in self.walk_children(Collapsible):
             child.collapsed = collapse
 
+class ChatResponse(Static):
+    def compose(self) -> ComposeResult:
+        with Vertical():
+            with ScrollableContainer():
+                yield Static("Ask Bot", id="chat-bot-response-button")
+                yield Static("Loading...", id="chat-bot-response")
+
+    def on_mount(self) -> None:
+        pass
+
+class ChatInput(Static):
+    BINDINGS = [
+        ("Enter", "send_message", "Send Message"),
+    ]
+    def compose(self) -> ComposeResult:
+        with Vertical():
+            yield TextArea(id="chat-input")
+
+    def on_mount(self) -> None:
+        pass
+
+    def action_send_message(self) -> None:
+        pass
+
+class ChatComponent(Static):
+    def compose(self) -> ComposeResult:
+        with Vertical():
+            yield TextArea(id="chat-input")
+            yield ChatResponse()
+
+    def on_mount(self) -> None:
+        pass
 
 class MainApp(App):
     CSS_PATH = "grid.tcss"
     def compose(self) -> ComposeResult:
-        yield Footer()
         yield ScrollableContainer(Static("All Emails"),CollapsibleApp())
-        yield Static("Two", classes="box")
+        yield Vertical(Static("Chat Bot"), ChatComponent(classes="box"))
         yield ScrollableContainer(Static("All Events"),TableApp())
-        
+        yield Footer()
+
 
 if __name__ == "__main__":
     app = MainApp()
